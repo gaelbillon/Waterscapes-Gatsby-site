d01b5fdafb958e884c43f557e476ff806c9de67b
Gaël Billon <gael@MacBook-Air-de-Gael.local>
2018-11-07 09:39:28 +0100

    Update src/utils/siteConfig.js
    siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,21 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
+  siteTitle: 'Waterscapes',
+  siteTitleAlt: 'Waterscapes directory', // This allows an alternative site title for SEO schema.
+  publisher: 'Gaël Billon', // Organization name used for SEO schema
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
   siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 41, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 40, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
+  author: 'Gaël Billon', // Author for RSS author segment and SEO schema
   authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@twitter', // Change for Twitter Cards
-  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: 'Watrscapes', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+  copyright: 'Copyright © 2018 Gaël Billon', // Copyright string for the RSS feed
 }
